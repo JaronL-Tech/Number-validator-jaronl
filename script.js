2b55bb1943a9b6c33dee9a6ac48b8e5f5ed96c2a
Jaron Leach <jaronleach98@gmail.com>
2024-08-18 08:35:07 -0400

    created the initial regenx and outputarray for my number.

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const userInput = document.getElementById("user-input");
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
 const output = document.getElementById("results-div");
+
+const regex = /^1? ?(\(\d{3}\)|\d{3})[ -]?\d{3}[ -]?\d{4}$/;
+const outputArr = [];
