2977be6f9dc60c2d7b3348203ee195376868773d
Jaron Leach <jaronleach98@gmail.com>
2024-08-19 04:25:19 -0400

    started working on the checkinput function that allows the for the valid number expression.

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const output = document.getElementById("results-div");
 
 const regex = /^1? ?(\(\d{3}\)|\d{3})[ -]?\d{3}[ -]?\d{4}$/;
 const outputArr = [];
+
+const checkInput = (str) => {
+  if (userInput.value === "") {
+    alert("Please provide a phone number");
+  } else if (regex.test(str)) {
+    outputArr.push(`Valid US number: ${str}`);
+    updateOutput(outputArr);
+  } else {
+    outputArr.push(`Invalid US number: ${str}`);
+    updateOutput(outputArr);
+  }
+};
